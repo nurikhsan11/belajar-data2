ac110522e47ee2bd4e0f10779bca0d1dcfe3ffd1
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-04-11 10:38:38 +0700

    project analisis data covid 19 di indonesia using python

---
+++ analisis data covid 19 di indonesia/data1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import requests
+resp_jabar = requests.get('https://storage.googleapis.com/dqlab-dataset/prov_detail_JAWA_BARAT.json', verify=False)
+cov_jabar_raw = resp_jabar.json()
+cov_jabar = pd.DataFrame(cov_jabar_raw['list_perkembangan'])
+
+cov_jabar_tidy = (cov_jabar.drop(columns=[item for item in cov_jabar.columns 
+                                               if item.startswith('AKUMULASI') 
+                                                  or item.startswith('DIRAWAT')])
+                           .rename(columns=str.lower)
+                           .rename(columns={'kasus': 'kasus_baru'})
+                  )
+cov_jabar_tidy['tanggal'] = pd.to_datetime(cov_jabar_tidy['tanggal']*1e6, unit='ns')
+
+import matplotlib.pyplot as plt
+
+plt.clf()
+fig, ax = plt.subplots(figsize=(10,5))
+ax.bar(data=cov_jabar_tidy, x='tanggal', height='kasus_baru')
+plt.show()
